e00950b7a6c498802fb774584f7c2279da0506f1
khairwaraman <aman.khairwar@wealth-right.com>
2025-03-19 20:27:06 +0530

    [Venue-Management-App]-Removed unwanted getter,setter methods

--- src/main/java/com/venue/mgmt/response/ApiResponse.java
+++ src/main/java/com/venue/mgmt/response/ApiResponse.java
@@ -1,9 +1,7 @@
 package com.venue.mgmt.response;
 
-import com.venue.mgmt.entities.LeadRegistration;
 import com.venue.mgmt.entities.Venue;
 import lombok.Data;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -13,55 +11,8 @@ public class ApiResponse<T>{
     private String statusMsg;
     private String errorMsg;
     private List<?> response;
-
-
     private Venue venueDetails; // Add this field
-
     private PaginationDetails paginationDetails; // Add this field
-//    public Venue getVenueDetails() {
-//        return venueDetails;
-//    }
-//
-//    public void setVenueDetails(Venue venueDetails) {
-//        this.venueDetails = venueDetails;
-//    }
-
-
-//    public ApiResponse() {
-//    }
-
-
-//    public int getStatusCode() {
-//        return statusCode;
-//    }
-//
-//    public void setStatusCode(int statusCode) {
-//        this.statusCode = statusCode;
-//    }
-//
-//    public String getStatusMsg() {
-//        return statusMsg;
-//    }
-//
-//    public void setStatusMsg(String statusMsg) {
-//        this.statusMsg = statusMsg;
-//    }
-//
-//    public String getErrorMsg() {
-//        return errorMsg;
-//    }
-//
-//    public void setErrorMsg(String errorMsg) {
-//        this.errorMsg = errorMsg;
-//    }
-//
-//    public List<?> getResponse() {
-//        return response;
-//    }
-//
-//    public void setResponse(List<?> response) {
-//        this.response = response;
-//    }
 
 
 
